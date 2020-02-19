f2a872758bf97caccf5bc2578563ce1daa181a4c
Your Name <you@example.com>
2020-02-19 21:16:36 +0100

    first commit

---
+++ src/hello_world/Hello_world.java
@@ -0,0 +1,17 @@
+package hello_world;
+
+
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!");
+System.out.println("testing");
+System.out.println(" app");
+System.out.println("Testing app with Selenium");
+System.out.println(123);
+
+	}
+
+}
